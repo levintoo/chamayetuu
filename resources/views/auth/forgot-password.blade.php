69db65be83c86bd7f94e215eebdc75e573a2e57b
levintoo <tooklevin@gmail.com>
2022-01-30 14:35:15 +0300

    reg+login pages remastered

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,37 +1,164 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+    <div class="auth-page">
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <div class="col-xxl-3 col-lg-4 col-md-5">
+                    <div class="auth-full-page-content d-flex p-sm-5 p-4">
+                        <div class="w-100">
+                            <div class="d-flex flex-column h-100">
+                                <div class="mb-4 mb-md-5 text-center">
+                                    <a href="/" class="d-block auth-logo">
+                                        <img src="{{ asset('assets/dashboard/assets/images/logo-sm.svg') }}" alt="" height="28"> <span class="logo-txt">Minia</span>
+                                    </a>
+                                </div>
+                                <div class="auth-content my-auto">
+                                    <div class="text-center">
+                                        <h5 class="mb-0">Reset Password</h5>
+                                        <p class="text-muted mt-2">Reset Password with Minia.</p>
+                                    </div>
+                                    <div class="alert alert-success text-center my-4" role="alert">
+                                        Enter your Email and instructions will be sent to you!
+                                    </div>
+                                    <x-jet-validation-errors class="mb-4 text-danger" />
+                                    <form class="mt-4" method="POST" action="{{ route('password.email') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input type="text" class="form-control" id="email" placeholder="Enter email" type="email" name="email" :value="old('email')" required autofocus >
+                                        </div>
+                                        <div class="mb-3 mt-4">
+                                            <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Reset</button>
+                                        </div>
+                                    </form>
 
-        <div class="card-body">
+                                    <div class="mt-5 text-center">
+                                        <p class="text-muted mb-0">Remember It ?  <a href="{{ route('login') }}"
+                                                                                     class="text-primary fw-semibold"> Sign In </a> </p>
+                                    </div>
+                                </div>
+                                <div class="mt-4 mt-md-5 text-center">
+                                    <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Minia   . Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end auth full page content -->
+                </div>
+                <!-- end col -->
+                <div class="col-xxl-9 col-lg-8 col-md-7">
+                    <div class="auth-bg pt-md-5 p-4 d-flex">
+                        <div class="bg-overlay bg-primary"></div>
+                        <ul class="bg-bubbles">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <!-- end bubble effect -->
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-xl-7">
+                                <div class="p-0 p-sm-4 px-xl-0">
+                                    <div id="reviewcarouselIndicators" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-indicators carousel-indicators-rounded justify-content-start ms-0 mb-0">
+                                            <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                            <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                            <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                        </div>
+                                        <!-- end carouselIndicators -->
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <div class="testi-contain text-white">
+                                                    <i class="bx bxs-quote-alt-left text-success display-6"></i>
 
-            <div class="mb-3">
-                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                                                    <h4 class="mt-4 fw-medium lh-base text-white">“I feel confident
+                                                        imposing change
+                                                        on myself. It's a lot more progressing fun than looking back.
+                                                        That's why
+                                                        I ultricies enim
+                                                        at malesuada nibh diam on tortor neaded to throw curve balls.”
+                                                    </h4>
+                                                    <div class="mt-4 pt-3 pb-5">
+                                                        <div class="d-flex align-items-start">
+                                                            <div class="flex-shrink-0">
+                                                                <img src="{{ asset('assets/dashboard/assets/images/users/avatar-1.jpg') }}" class="avatar-md img-fluid rounded-circle" alt="...">
+                                                            </div>
+                                                            <div class="flex-grow-1 ms-3 mb-4">
+                                                                <h5 class="font-size-18 text-white">Richard Drews
+                                                                </h5>
+                                                                <p class="mb-0 text-white-50">Web Designer</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="carousel-item">
+                                                <div class="testi-contain text-white">
+                                                    <i class="bx bxs-quote-alt-left text-success display-6"></i>
+
+                                                    <h4 class="mt-4 fw-medium lh-base text-white">“Our task must be to
+                                                        free ourselves by widening our circle of compassion to embrace
+                                                        all living
+                                                        creatures and
+                                                        the whole of quis consectetur nunc sit amet semper justo. nature
+                                                        and its beauty.”</h4>
+                                                    <div class="mt-4 pt-3 pb-5">
+                                                        <div class="d-flex align-items-start">
+                                                            <div class="flex-shrink-0">
+                                                                <img src="{{ asset('assets/dashboard/assets/images/users/avatar-2.jpg') }}" class="avatar-md img-fluid rounded-circle" alt="...">
+                                                            </div>
+                                                            <div class="flex-grow-1 ms-3 mb-4">
+                                                                <h5 class="font-size-18 text-white">Rosanna French
+                                                                </h5>
+                                                                <p class="mb-0 text-white-50">Web Developer</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="carousel-item">
+                                                <div class="testi-contain text-white">
+                                                    <i class="bx bxs-quote-alt-left text-success display-6"></i>
+
+                                                    <h4 class="mt-4 fw-medium lh-base text-white">“I've learned that
+                                                        people will forget what you said, people will forget what you
+                                                        did,
+                                                        but people will never forget
+                                                        how donec in efficitur lectus, nec lobortis metus you made them
+                                                        feel.”</h4>
+                                                    <div class="mt-4 pt-3 pb-5">
+                                                        <div class="d-flex align-items-start">
+                                                            <img src="{{ asset('assets/dashboard/assets/images/users/avatar-3.jpg') }}"
+                                                                 class="avatar-md img-fluid rounded-circle" alt="...">
+                                                            <div class="flex-1 ms-3 mb-4">
+                                                                <h5 class="font-size-18 text-white">Ilse R. Eaton</h5>
+                                                                <p class="mb-0 text-white-50">Manager
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- end carousel-inner -->
+                                    </div>
+                                    <!-- end review carousel -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end col -->
             </div>
-
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            <x-jet-validation-errors class="mb-3" />
-
-            <form method="POST" action="/forgot-password">
-                @csrf
-
-                <div class="mb-3">
-                    <x-jet-label value="Email" />
-                    <x-jet-input type="email" name="email" :value="old('email')" required autofocus />
-                </div>
-
-                <div class="d-flex justify-content-end mt-4">
-                    <x-jet-button>
-                        {{ __('Email Password Reset Link') }}
-                    </x-jet-button>
-                </div>
-            </form>
+            <!-- end row -->
         </div>
-    </x-jet-authentication-card>
+        <!-- end container fluid -->
+    </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,29 +1,76 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        @livewireStyles
+<!doctype html>
+<html lang="en">
 
-        @livewireScripts
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+        <title>Chamayetu Reg Dashboard</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/dashboard/assets/images/favicon.ico') }}">
 
-        @stack('styles')
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <!-- preloader css -->
+{{--    <link rel="stylesheet" href="{{ asset('assets/dashboard/assets/css/preloader.min.css') }}" type="text/css" />--}}
+    <!-- datepicker css -->
+    <link rel="stylesheet" href="{{ asset('assets/dashboard/assets/libs/flatpickr/flatpickr.min.css') }}">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/dashboard/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/dashboard/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/dashboard/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- choices js -->
+    <script src="{{ asset('assets/dashboard/assets/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
 
-        <script src="{{ mix('js/app.js') }}" defer></script>
+    <!-- color picker css -->
+    <link rel="stylesheet" href="{{ asset('assets/dashboard/assets/libs/%40simonwep/pickr/themes/classic.min.css') }}"/> <!-- 'classic' theme -->
+    <link rel="stylesheet" href="{{ asset('assets/dashboard/assets/libs/%40simonwep/pickr/themes/monolith.min.css') }}"/> <!-- 'monolith' theme -->
+    <link rel="stylesheet" href="{{ asset('assets/dashboard/assets/libs/%40simonwep/pickr/themes/nano.min.css') }}"/> <!-- 'nano' theme -->
 
-    </head>
-    <body class="bg-light font-sans antialiased">
-        {{ $slot }}
-    </body>
-    <!-- Scripts -->
+    <!-- datepicker css -->
+    <link rel="stylesheet" href="{{ asset('assets/dashboard/assets/libs/flatpickr/flatpickr.min.css') }}">
+    @livewireStyles
+    @stack('guest-styles')
+</head>
+
+<body>
+
+{{$slot}}
+
+<!-- JAVASCRIPT -->
+
+<script src="{{ asset('assets/dashboard/assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/dashboard/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/dashboard/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ asset('assets/dashboard/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/dashboard/assets/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('assets/dashboard/assets/libs/feather-icons/feather.min.js') }}"></script>
+<!-- pace js -->
+<script src="{{ asset('assets/dashboard/assets/libs/pace-js/pace.min.js') }}"></script>
+<!-- password addon init -->
+<script src="{{ asset('assets/dashboard/assets/js/pages/pass-addon.init.js') }}"></script>
+
+<!-- choices js -->
+<script src="{{ asset('assets/dashboard/assets/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+
+
+
+<!-- color picker js -->
+<script src="{{ asset('assets/dashboard/assets/libs/%40simonwep/pickr/pickr.min.js') }}"></script>
+<script src="{{ asset('assets/dashboard/assets/libs/%40simonwep/pickr/pickr.es5.min.js') }}"></script>
+<!-- color picker js -->
+
+<!-- datepicker js -->
+<script src="{{ asset('assets/dashboard/assets/libs/flatpickr/flatpickr.min.js') }}"></script>
+
+<!-- init js -->
+<script src="{{ asset('assets/dashboard/assets/js/pages/form-advanced.init.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+@livewireScripts
+@stack('guest-scripts')
+</body>
 
 </html>

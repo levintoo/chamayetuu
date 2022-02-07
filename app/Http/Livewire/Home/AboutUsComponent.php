24aad505430c5050f62c888e2ec60c3381e8dc39
levintoo <tooklevin@gmail.com>
2022-02-07 20:27:40 +0300

    home components

---
+++ app/Http/Livewire/Home/AboutUsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Home;
+
+use Livewire\Component;
+
+class AboutUsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.home.about-us-component');
+    }
+}
---
+++ app/Http/Livewire/Home/ContactComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Home;
+
+use Livewire\Component;
+
+class ContactComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.home.contact-component');
+    }
+}
